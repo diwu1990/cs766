43f59758eb894a7d1ab54caebc688c9e272b9240
Bin LI <mushanbin.li@wisc.edu>
2019-03-25 16:41:10 -0500

    correct syntax, add sigmoid

--- model.py
+++ model.py
@@ -7,9 +7,11 @@ class Net(nn,Module):
 		super(Net, self).__init__()
         
         self.block1 = nn.Sequential(
+            # input image 96x96
             nn.Conv2d(
                 in_channels=1, out_channels=64, kernel_size=7, stride=2
             )
+            # image 48x48
             nn.BatchNorm2d(64)
             nn.LeakyReLU(0.1) # parameters
             nn.Conv2d(
@@ -23,17 +25,38 @@ class Net(nn,Module):
             nn.BatchNorm2d(64)
             nn.LeakyReLU(0.1) # parameters
         )
+        
+        self.block2 = nn.Sequential(
+            # image 48x48
+            nn.Conv2d(
+                in_channels=64, out_channels=64, kernel_size=3, stride=1
+            )
+            nn.BatchNorm2d(64)
+            nn.LeakyReLU(0.1) # parameters
+            nn.Conv2d(
+                in_channels=64, out_channels=64, kernel_size=3, stride=1
+            )
+            nn.BatchNorm2d(64)
+            nn.LeakyReLU(0.1) # parameters
+        )
+        
+        # TODO: implement all blocks
+        
+        
 		
         
 	def forward(self, x):
         
 		residual1 = x # save input as residual
-		out = self.block1 # run block1
+		out = self.block1(x) # run block1
         out = out + residual # add residuak to output
         
         residual2 = out # save block1 output as new residual
-        out = sefl.block2
+        out = sefl.block2(out)
         out = out + residual2
+        
+        # TODO: implement all block forward
+        out = F.sigmoid(out) # fully connect sigmoid, image 96x96      
         
 		return out
     

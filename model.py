5968191dab7001b4541dc75b86b5baeedb63a32b
Bin Li <bli346@wisc.edu>
2019-03-26 15:00:30 -0500

    fix padding, add SSIM loss

--- model.py
+++ model.py
@@ -95,13 +95,14 @@ class Net(nn.Module):
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.1), # parameters
             # image 12x12
+            # padding till here is all correct
         
         )
         
         self.block5 = nn.Sequential(
             # image 12x12
             nn.ConvTranspose2d(
-                in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1
+                in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1
             ),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.1), # parameters
@@ -110,13 +111,14 @@ class Net(nn.Module):
                 in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1
             ),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.1), # parameters    
+            nn.LeakyReLU(0.1), # parameters  
+            # image 24x24
         )
         
         self.side5 = nn.Sequential(
             # image 12x12
             nn.ConvTranspose2d(
-                in_channels=256, out_channels=128, kernel_size=1, stride=2
+                in_channels=256, out_channels=128, kernel_size=1, stride=2, output_padding=1
             ),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.1), # parameters
@@ -126,7 +128,7 @@ class Net(nn.Module):
         self.block6 = nn.Sequential(
             # image 24x24
             nn.ConvTranspose2d(
-                in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1
+                in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1
             ),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1), # parameters
@@ -135,13 +137,14 @@ class Net(nn.Module):
                 in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1
             ),
             nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1), # parameters    
+            nn.LeakyReLU(0.1), # parameters
+            # image 48x48
         )
         
         self.side6 = nn.Sequential(
             # image 24x24
             nn.ConvTranspose2d(
-                in_channels=128, out_channels=64, kernel_size=1, stride=2
+                in_channels=128, out_channels=64, kernel_size=1, stride=2, output_padding=1
             ),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1), # parameters
@@ -151,7 +154,7 @@ class Net(nn.Module):
         self.block7 = nn.Sequential(
             # image 48x48
             nn.ConvTranspose2d(
-                in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1
+                in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1
             ),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1), # parameters
@@ -159,9 +162,9 @@ class Net(nn.Module):
         )
         
         self.block8 = nn.Sequential(
-            # image 48x48
+            # image 96x96
             nn.ConvTranspose2d(
-                in_channels=32, out_channels=1, kernel_size=3, stride=1, padding=1
+                in_channels=32, out_channels=1, kernel_size=1, stride=1
             ),
             nn.BatchNorm2d(1),
             nn.LeakyReLU(0.1), # parameters
@@ -210,7 +213,7 @@ class Net(nn.Module):
 
         out = self.fc(out)
         
-        out = nn.functional.interpolate(out, [96, 96])
+#         out = nn.functional.interpolate(out, [96, 96])
         
 #         out = F.relu(out) # fully connect sigmoid, image 96x96      
         
